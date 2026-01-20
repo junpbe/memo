96c908fc6d1d6e05c9edd2ca30471353f1f9352e
junpbe <wind.project.whence@gmail.com>
2026-01-20 11:29:56 +0900

    メモテーブル作成

---
+++ database/migrations/2026_01_19_000000_create_memos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('memos', function (Blueprint $table) {
+            $table->comment('メモ');
+
+            $table->id()->comment('サロゲートキー');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete()->comment('ユーザID:users.id');
+            $table->text('memo')->nullable()->comment('メモ');
+            $table->datetimes(6);
+        });
+
+        Schema::table('memos', function (Blueprint $table) {
+            $table->foreignId('created_by')->after('created_at')->comment('作成者');
+            $table->foreignId('updated_by')->after('updated_at')->comment('更新者');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('memos');
+    }
+};
