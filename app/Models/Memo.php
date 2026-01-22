841a93154db4905a99848c3e56356c62a49d134d
junpbe <wind.project.whence@gmail.com>
2026-01-22 11:24:09 +0900

    メモモデルとユーザモデルのリレーション作成

--- app/Models/Memo.php
+++ app/Models/Memo.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 use App\Models\Traits\Creatable;
 use App\Models\Traits\Updatable;
+use Illuminate\Database\Eloquent\Model;
 
 /**
  * メモモデル。
  */
-class Memo
+class Memo extends Model
 {
     use Creatable, Updatable;
 
