16219278f20dac43d5c8c3b86f87bc61032ff5c0
junpbe <wind.project.whence@gmail.com>
2026-02-25 17:39:43 +0900

    env追加

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,7 @@
 # envファイル複合化
 if [ -n "$LARAVEL_ENV_ENCRYPTION_KEY" ]; then
     echo "Decrypting environment file..."
-    php artisan env:decrypt --key="$LARAVEL_ENV_ENCRYPTION_KEY" --force
+    php artisan env:decrypt --key="$LARAVEL_ENV_ENCRYPTION_KEY" --env="${APP_ENV}"
 fi
 
 # 元のentrypointを実行
