2801dc2bb222abdaf0f6f4bc44c60949eacca103
junpbe <wind.project.whence@gmail.com>
2026-02-25 18:42:35 +0900

    env追加

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,5 +6,8 @@ if [ -n "${LARAVEL_ENV_ENCRYPTION_KEY}" ]; then
     php artisan env:decrypt --key="${LARAVEL_ENV_ENCRYPTION_KEY}" --env="${APP_ENV}"
 fi
 
+# laravel最適化（env複合化後に実行しないとenvの内容が反映されない）
+php artisan optimize
+
 # 元のentrypointを実行
 exec docker-php-entrypoint "$@"
