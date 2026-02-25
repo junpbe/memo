1eb31e29a82ab3629bf74ceba50e01fba68d05f6
junpbe <wind.project.whence@gmail.com>
2026-02-25 15:36:35 +0900

    sqliteファイル共有化 初期化シェル作成

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'test1',
+            'email' => 'test1@example.com',
+            'password' => '',
         ]);
     }
 }
