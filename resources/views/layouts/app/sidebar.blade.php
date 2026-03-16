86928b43693abdc7c1e5b6e91a118422d77476f6
junpbe <wind.project.whence@gmail.com>
2026-03-16 13:34:23 +0900

    左メニュー整理

--- resources/views/layouts/app/sidebar.blade.php
+++ resources/views/layouts/app/sidebar.blade.php
@@ -14,12 +14,14 @@
                 <flux:sidebar.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>
                     {{ __('Dashboard') }}
                 </flux:sidebar.item>
-                <flux:sidebar.item icon="chevron-right" :href="route('simple')" :current="request()->routeIs('simple')" wire:navigate>
-                    簡易編集
-                </flux:sidebar.item>
-                <flux:sidebar.item icon="chevron-right" :href="route('normal')" :current="request()->routeIs('normal')" wire:navigate>
-                    通常編集
-                </flux:sidebar.item>
+                <flux:sidebar.group expandable heading="メモ" class="grid">
+                    <flux:sidebar.item :href="route('simple')" :current="request()->routeIs('simple')" wire:navigate>
+                        簡易編集
+                    </flux:sidebar.item>
+                    <flux:sidebar.item :href="route('normal')" :current="request()->routeIs('normal')" wire:navigate>
+                        通常編集
+                    </flux:sidebar.item>
+                </flux:sidebar.group>
             </flux:sidebar.nav>
 
             <flux:spacer />
